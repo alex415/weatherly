b578fff55182be4b4f9abf4dc4ab136f10e09ac1
Alex Chan <alex415@gmail.com>
2015-10-06 00:03:09 -0700

    fixed html and body width

---
+++ README.md
@@ -0,0 +1,11 @@
+Installation:
+
+You will need http-server installed to run this locally.
+
+1. Install http-server:
+  
+  sudo npm install http-server -g
+
+2. In the terminal, navigate to the "weather_app" folder and enter "http-server"
+
+3. Open web browser and enter "localhost:8080" to view the project.
--- app.js
+++ app.js
@@ -5,11 +5,7 @@ angular.module('weatherApp', ['ngRoute', 'ngAutocomplete'])
     .when('/', {
       templateUrl: 'main.html',
       controller: 'MainCtrl'
-    })
-    .otherwise({
-      redirectTo: '/'
     });
-
   $locationProvider.html5Mode({
     enabled: true,
     requireBase: false
@@ -18,17 +14,49 @@ angular.module('weatherApp', ['ngRoute', 'ngAutocomplete'])
 
 .controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
 
-  $scope.searchResults = '';
+  // options for autocomplete
+  $scope.options = {
+    country: '',
+    types: '(cities)'
+  };
 
+  // default city set to san francisco
+  $scope.searchResult = 'San Francisco';
+
+  // call to api on form submit
   $scope.submit = function () {
-    var url = 'http://api.openweathermap.org/data/2.5/forecast?q=' + $scope.searchResult + ',us&units=imperial&APPID=5805096c65c2190f6c9fd7c4f90dd96c';
+
+    var url = 'http://api.openweathermap.org/data/2.5/forecast?q=' + $scope.searchResult + '&units=imperial&type=accurate&APPID=5805096c65c2190f6c9fd7c4f90dd96c';
     $http.get(url)
       .then(function(response) {
-        $scope.cityResults = response.data;
+
+        // alert for no city found
         var status = response.data.cod;
           if (status === "404") {
             alert("No city found");
           }
+
+        // today's data
+        $scope.cityName = response.data.city.name;
+        $scope.todayIcon = response.data.list[2].weather[0].id;
+        $scope.todayTemp = Math.round(response.data.list[2].main.temp);
+
+        // future data 1
+        $scope.futureIcon1 = response.data.list[10].weather[0].id;
+        $scope.futureTemp1 = Math.round(response.data.list[10].main.temp);
+        $scope.futureDate1 = moment(response.data.list[10].dt_txt).format('ddd MMM D');
+        
+        // future data 2
+        $scope.futureIcon2 = response.data.list[18].weather[0].id; 
+        $scope.futureTemp2 = Math.round(response.data.list[18].main.temp);
+        $scope.futureDate2 = moment(response.data.list[18].dt_txt).format('ddd MMM D');
+
+        // future data 3
+        $scope.futureIcon3 = response.data.list[27].weather[0].id;
+        $scope.futureTemp3 = Math.round(response.data.list[27].main.temp);
+        $scope.futureDate3 = moment(response.data.list[27].dt_txt).format('ddd MMM D');
+
+         // error handling
       }, function(response) {
             var error = response.status;
               if (error === 404 || 408) {
@@ -37,23 +65,7 @@ angular.module('weatherApp', ['ngRoute', 'ngAutocomplete'])
       });
   };
 
-  // $scope.images = [
-  //   { src : 'thunderstorm.png' },
-  //   { src : 'drizzle.png' },
-  //   { src : 'rain.png' },
-  //   { src : 'snow.png' },
-  //   { src : 'atmosphere.png' },
-  //   { src : 'clouds.png' },
-  //   { src : 'extreme.png' },
-  //   { src : 'additional.png' }
-  // ];
-
-
-  // if ($scope.result BETWEEN 1 TO 100)
-  //     $scope.img = "icon1.png"
-
-  // if ($scope.result BETWEEN 101 TO 200)
-  //     $scope.img= ="icon2.png"
-
+  // get default data on page load
+  $scope.submit();
 
 }]); // end
